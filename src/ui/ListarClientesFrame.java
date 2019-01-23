b98bd14ddda1f04d403ebcd978df6d62f4d56a67
Adjamilton Junior <jr@ieee.org>
2019-01-23 06:37:17 -0300

    atualizacao

--- src/ui/ListarClientesFrame.java
+++ src/ui/ListarClientesFrame.java
@@ -5,14 +5,14 @@ import modelo.Cliente;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class ListarClientesFrame extends JDialog {
 
     private JPanel contentPanel;
+    private JTable tblListaClientes;
     private JLabel lblQtdeClientes;
     private JLabel lblQtde;
     private JTextArea textAreaListaClientes;
@@ -24,17 +24,35 @@ public class ListarClientesFrame extends JDialog {
         setTitle("Lista de Clientes");
         setResizable(false);
         setLocationRelativeTo(null);
-        //setBackground(MaterialColors.WHITE);
-        //setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         contentPanel = new JPanel();
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPanel);
         contentPanel.setLayout(null);
 
-        textAreaListaClientes = new JTextArea();
-        textAreaListaClientes.setBounds(10, 10, 565, 400);
-        contentPanel.add(textAreaListaClientes);
+        String[] tblHeader = { "Telefone", "Nome", "Email", "Endereço" };
+
+        ArrayList<Cliente> resultado = Fachada.listarClientes();
+
+        DefaultTableModel model = new DefaultTableModel(tblHeader,resultado.size());
+        tblListaClientes = new JTable(model);
+        tblListaClientes.setBounds(10, 10, 150, 150);
+        contentPanel.add(tblListaClientes);
+
+        int linha = 0;
+        for (Cliente c : resultado) {
+            tblListaClientes.setValueAt(c.getTelefone(), linha, 0);
+            tblListaClientes.setValueAt(c.getNome(), linha, 1);
+            tblListaClientes.setValueAt(c.getEmail(), linha, 2);
+            tblListaClientes.setValueAt(c.getEndereco(), linha, 3);
+            linha++;
+        }
+
+        JScrollPane jsp = new JScrollPane(tblListaClientes);
+        jsp.setBounds(10, 10, 565, 415);
+        contentPanel.add(jsp);
+        jsp.setViewportView(tblListaClientes);
 
         lblQtdeClientes = new JLabel("Quantidade de Clientes:");
         lblQtdeClientes.setBounds(10, 430, 150, 20);
@@ -44,17 +62,6 @@ public class ListarClientesFrame extends JDialog {
         lblQtde.setBounds(155, 430, 80, 20);
         contentPanel.add(lblQtde);
 
-        ArrayList<Cliente> resultado = Fachada.listarClientes();
         lblQtde.setText(Integer.toString(resultado.size()));
-
-        String texto = "Listagem de Clientes: \n";
-
-        if (resultado.isEmpty())
-            texto += "Nenhum cliente cadastrado\n";
-        else
-            for (Cliente c : resultado)
-                texto += c + "\n";
-
-        textAreaListaClientes.setText(texto);
     }
 }
